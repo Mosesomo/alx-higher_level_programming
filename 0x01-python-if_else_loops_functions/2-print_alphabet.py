c205226f0d26f20b765c1422eaebf001cac33785
Mosesomo <moseswamboga4@gmail.com>
2023-08-08 10:23:19 -0700

    lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,6 +2,6 @@
 # 2-print_alphabet.py
 
 
-"""prints alphabets in lowercase ignoring the new line."""
+"""Print alphabets in lowercase ignoring the new line."""
 for alpha in range(97, 123):
     print(chr(alpha), end="")

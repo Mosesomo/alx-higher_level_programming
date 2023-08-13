e0ef86cc9e4ac409ad5acf1dd95fe34945efd2ab
Mosesomo <moseswamboga4@gmail.com>
2023-08-13 14:37:02 -0700

    find max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     max_int = my_list[0]
     for num in my_list:
-            if num > max_int:
-                max_int = num
+        if num > max_int:
+            max_int = num
     return max_int

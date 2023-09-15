31bc2d67e249778f53480640e3494697f4b0cd2b
Mosesomo <moseswamboga4@gmail.com>
2023-09-15 09:11:37 -0700

    Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,6 +16,17 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Retrieves the size of the square"""
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Returns the string representation of a square"""
 

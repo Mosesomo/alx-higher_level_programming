a966034407295fdb09f9248b78eecda15453cfdb
Mosesomo <moseswamboga4@gmail.com>
2024-01-05 02:33:15 -0800

    webpage

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require("request");
+const fs = require("fs");
+const url = process.argv[2];
+const path = process.argv[3];
+request.get(url, (err, response, body) => {
+	if (err){
+		console.error(err);
+		return;
+	}
+	if (response.statusCode >= 200 && response.statusCode < 300){
+		fs.writeFile(path, body, "utf-8",  (err) => {
+			if (err){
+				console.error(err);
+				return;
+			}
+			return (path);
+		});
+	}
+});
